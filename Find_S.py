9bd6536665b7ee38cee4cf99fa37a2288c6ea063
Adith <109802085+Adith66@users.noreply.github.com>
2023-05-06 09:30:26 +0530

    Add files via upload

---
+++ Find_S.py
@@ -0,0 +1,29 @@
+import csv
+with open("C:/Users/Adi/Downloads/hello.csv") as csv_file:
+    csvread=csv.reader(csv_file,delimiter=',')
+    x=[];y=[];z=[];s=[]
+    print('the training dataset is:')
+    for row in csvread:
+        x.append(row)
+        print(row)
+    if x[0][-1].upper()!='YES'and'NO':
+        x.remove(x[0])
+    print('the positive examples are:')
+    for i in range(len(x)):
+        if x[i][-1].upper()=='YES':
+            z.append(x[i])
+            print(x[i])
+    y=z
+    for i in range(len(y)):
+        y[i].remove(y[i][-1])
+    print('the steps in algorithm are:')
+    for i in y:
+        for j in range(len(i)):
+            if s==[]:
+                s=['%']*len(i)
+                print(s)
+                s=y[0]
+            elif s[j]!=i[j]:
+                s[j]='?'
+        print(s)
+    print('the most general hypothesis is:',s)
